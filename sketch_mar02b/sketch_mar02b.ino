07af6a87e6cb5bcbcfa6c1191ea3dfddf748e0c3
jordane-quincy <jordanequincy@gmail.com>
2016-03-02 18:24:31 +0100

    20160302_1825

--- sketch_mar02b/sketch_mar02b.ino
+++ sketch_mar02b/sketch_mar02b.ino
@@ -2,9 +2,7 @@
 #include <AndroidAccessory.h>
 
 
-#define  LED3_RED       2
-#define  LED3_GREEN     4
-#define  LED3_BLUE      3
+#define LED0_INTEGRATED 13
 
 AndroidAccessory acc("Google, Inc.",
 		     "DemoKit",
@@ -16,46 +14,55 @@ AndroidAccessory acc("Google, Inc.",
 void setup();
 void loop();
 
-void init_pin()
-{
-	pinMode(BUTTON1, INPUT);
-	pinMode(BUTTON2, INPUT);
-	pinMode(BUTTON3, INPUT);
-	pinMode(JOY_SWITCH, INPUT);
-
-	// enable the internal pullups
-	digitalWrite(BUTTON1, HIGH);
-	digitalWrite(BUTTON2, HIGH);
-	digitalWrite(BUTTON3, HIGH);
-	digitalWrite(JOY_SWITCH, HIGH);
+void init_pin(){
+  pinMode(LED0_INTEGRATED, OUTPUT);
 }
 
-void setup()
-{
-        Serial.print("\r\nStart");
-        Serial.begin(115200);
-	
-        Serial.print("\r\nInit pin");
-	init_pin();
-        Serial.print("\r\nAndroid power on");
-	acc.powerOn();
+void setup() {
+  Serial.print("\r\nStart");
+  Serial.begin(115200);
+  
+  Serial.print("\r\nInit pin");
+  init_pin();
+  
+  Serial.print("\r\nAndroid power on");
+  acc.powerOn();
 }
 
-void loop()
-{
+void blinkLedIntegratedXtimes(int x){
+  for (int i=0; i < x; i++){
+    digitalWrite(LED0_INTEGRATED, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(1000);              // wait for a second
+    digitalWrite(LED0_INTEGRATED, LOW);    // turn the LED off by making the voltage LOW
+    delay(1000);              // wait for a second
+  }
+}
+
+void loop() {
 	byte err;
 	byte idle;
 	static byte count = 0;
 	byte msg[4];
 	long touchcount;
+  int nbBlink = 0;
 
 	if (acc.isConnected()) {
-                Serial.print("\r\nAndroid is connected at : ");Serial.print (millis(),DEC);
-                msg[0] = 'a';
-                msg[1] = 'b';
-                msg[2] = 'c';
-                msg[3] = '\n';
-                acc.write(msg, sizeof(msg));
+    Serial.print("\r\nAndroid is connected at : ");Serial.print (millis(),DEC);
+    int len = acc.read(msg, sizeof(msg), 1);
+    if(len > 0){
+      //if some data received from the android
+      nbBlink = atoi((char*)msg[0]); //FIXME : comment récupérer l'entier au lieu du code ascii (si saisi "2" sur l'android, ne pas récuperer 50 dans nbBlink)
+      String log = String("\r\nIntegrated led will blink : ");
+      log.concat(nbBlink);
+      Serial.print(log);
+      blinkLedIntegratedXtimes(nbBlink);
+    }
+    
+    msg[0] = 'a';
+    msg[1] = 'b';
+    msg[2] = 'c';
+    msg[3] = '\n';
+    acc.write(msg, sizeof(msg));
 	} else {
             
                 Serial.print("\r\nAndroid is not plugged");
