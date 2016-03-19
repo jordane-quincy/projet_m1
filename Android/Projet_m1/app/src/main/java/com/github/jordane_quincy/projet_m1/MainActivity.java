241b0c3e89368b0dfe6d8b15bcdfe31561de6e74
jordane-quincy <jordanequincy@gmail.com>
2016-03-19 16:13:35 +0100

    160319_1613

--- Android/Projet_m1/app/src/main/java/com/github/jordane_quincy/projet_m1/MainActivity.java
+++ Android/Projet_m1/app/src/main/java/com/github/jordane_quincy/projet_m1/MainActivity.java
@@ -1,15 +1,51 @@
 package com.github.jordane_quincy.projet_m1;
 
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.usb.UsbAccessory;
+import android.hardware.usb.UsbManager;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+
+    // TAG is used to debug in Android logcat console
+    private static final String TAG = "ArduinoAccessory";
+
+    public UsbManager mUsbManager;
+    public PendingIntent mPermissionIntent;
+    public boolean mPermissionRequestPending;
+
+    public UsbAccessory mAccessory;
+    public ParcelFileDescriptor mFileDescriptor;
+    public FileInputStream mInputStream;
+    public FileOutputStream mOutputStream;
+
+    /*
+    UsbAccessory[] usbAccessoryTab = mUsbManager.getAccessoryList();
+    if(usbAccessoryTab == null){
+        Log.d("USB_CONNEXION", "usbAccessoryTab is null");
+    }else{
+        UsbAccessory usbAccessory = usbAccessoryTab[0];
+
+    }
+    */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +62,55 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Log.d(TAG, "Debut de la partie gestion usb");
+
+        mUsbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
+        mPermissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
+        IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
+        filter.addAction(UsbManager.ACTION_USB_ACCESSORY_DETACHED);
+
+        Log.d(TAG, "enregistrement receiver");
+                registerReceiver(mUsbReceiver, filter);
+        Log.d(TAG, " receiver enregistrer");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (mInputStream != null && mOutputStream != null) {
+            return;
+        }
+
+        UsbAccessory[] accessories = mUsbManager.getAccessoryList();
+        UsbAccessory accessory = (accessories == null ? null : accessories[0]);
+        if (accessory != null) {
+            if (mUsbManager.hasPermission(accessory)) {
+                openAccessory(accessory);
+            } else {
+                synchronized (mUsbReceiver) {
+                    if (!mPermissionRequestPending) {
+                        mUsbManager.requestPermission(accessory,mPermissionIntent);
+                        mPermissionRequestPending = true;
+                    }
+                }
+            }
+        } else {
+            Log.d(TAG, "mAccessory is null");
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        closeAccessory();
+    }
+
+    @Override
+    public void onDestroy() {
+        unregisterReceiver(mUsbReceiver);
+        super.onDestroy();
     }
 
     @Override
@@ -48,5 +133,71 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void closeAccessory() {
+        try {
+            if (mFileDescriptor != null) {
+                mFileDescriptor.close();
+            }
+        } catch (IOException e) {
+            Log.e(TAG,"Impossible to close accessory");
+        } finally {
+            mFileDescriptor = null;
+            mAccessory = null;
+        }
+    }
+
+    private static final String ACTION_USB_PERMISSION =
+            "com.android.example.USB_PERMISSION";
+
+    private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (ACTION_USB_PERMISSION.equals(action)) {
+                synchronized (this) {
+                    UsbAccessory accessory = getAccessory();//UsbManager.getAccessory(intent);
+                    if (intent.getBooleanExtra(
+                            UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                        openAccessory(accessory);
+                    } else {
+                        Log.d(TAG, "permission denied for accessory "
+                                + accessory);
+                    }
+                    mPermissionRequestPending = false;
+                }
+            } else if (UsbManager.ACTION_USB_ACCESSORY_DETACHED.equals(action)) {
+                UsbAccessory accessory = getAccessory(); //UsbManager.getAccessory(intent);
+                if (accessory != null && accessory.equals(mAccessory)) {
+                    closeAccessory();
+                }
+            }
+        }
+    };
+
+    private void openAccessory(UsbAccessory accessory) {
+        mFileDescriptor = mUsbManager.openAccessory(accessory);
+        if (mFileDescriptor != null) {
+            mAccessory = accessory;
+            FileDescriptor fd = mFileDescriptor.getFileDescriptor();
+            mInputStream = new FileInputStream(fd);
+            mOutputStream = new FileOutputStream(fd);
+            Log.d(TAG, "accessory opened");
+        } else {
+            Log.d(TAG, "accessory open fail");
+        }
+    }
+
+    private UsbAccessory getAccessory(){
+        UsbAccessory usbAccessory = null;
+        UsbAccessory[] usbAccessoryTab = mUsbManager.getAccessoryList();
+        if(usbAccessoryTab == null){
+            Log.d("USB_CONNEXION", "usbAccessoryTab is null");
+        }else{
+             usbAccessory = usbAccessoryTab[0];
+        }
+
+        return usbAccessory;
     }
 }
